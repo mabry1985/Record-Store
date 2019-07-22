7e064be6968b4f7e19f39224adc758bd99bb0989
Alex Siegberg and Daniel Hongel <alex@>
2019-07-22 14:00:01 -0700

    finish the lesson to add full CRUD functionality to the webpage.

--- app.rb
+++ app.rb
@@ -1,18 +1,66 @@
 require('sinatra')
 require('sinatra/reloader')
+require('./lib/album')
+require('pry')
 also_reload('lib/**/*.rb')
 
 get ('/') do
-  "This will be our home page. '/' is always the root route in a Sinatra application."
+  @albums = Album.all
+  erb(:albums)
 end
 
 get ('/albums') do
-  "This route will show a list of all albums."
+  @albums = Album.all
+  erb(:albums)
 end
 
 get ('/albums/new') do
-  "This will take us to a page with a form for adding a new album."
+  erb(:new_album)
 end
+
+post ('/albums') do
+  name = params[:album_name]
+  album = Album.new(name, nil)
+  album.save()
+  @albums = Album.all() # Adding this line will fix the error.
+  erb(:albums)
+end
+
+get ('/albums/:id') do
+  @album = Album.find(params[:id].to_i())
+  erb(:album)
+end
+
+get ('/albums/:id/edit') do
+  @album = Album.find(params[:id].to_i())
+  erb(:edit_album)
+end
+
+patch ('/albums/:id') do
+  @album = Album.find(params[:id].to_i())
+  @album.update(params[:name])
+  @albums = Album.all
+  erb(:albums)
+end
+
+delete ('/albums/:id') do
+  @album = Album.find(params[:id].to_i())
+  @album.delete()
+  @albums = Album.all
+  erb(:albums)
+end
+
+# get ('/') do
+#   "This will be our home page. '/' is always the root route in a Sinatra application."
+# end
+#
+# get ('/albums') do
+#   "This route will show a list of all albums."
+# end
+#
+# get ('/albums/new') do
+#   "This will take us to a page with a form for adding a new album."
+# end
 
 get ('/albums/:id') do
   "This route will show a specific album based on its ID. The value of ID here is #{params[:id]}."
